2df325023b841a3825212605109770637024e1b8
Lan Tran <tranl8@uci.edu>
2017-11-13 22:37:49 -0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function main() {
-  //$('.bar').hide();
-  $('.fa-list').on('click', function() {
-    $('.bar').slideToggle(700);
-    $('.bar').toggleClass('active');
-   });
-}
-	
-$(document).ready(main);
